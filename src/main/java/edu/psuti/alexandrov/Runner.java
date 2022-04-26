e594764e495a70f35ef71a890f380378b9f8c210
alexandrov <alexandrov@o-code.com>
2022-04-26 12:49:20 +0400

    Initial commit

---
+++ src/main/java/edu/psuti/alexandrov/Runner.java
@@ -0,0 +1,18 @@
+package edu.psuti.alexandrov;
+
+import ai.djl.translate.TranslateException;
+
+import java.io.IOException;
+
+public class Runner {
+
+
+
+    public static void main(String... args) throws TranslateException, IOException {
+        //https://docs.djl.ai/docs/development/how_to_use_dataset.html
+
+        //https://docs.djl.ai/jupyter/tutorial/01_create_your_first_network.html
+
+        //!! https://towardsdatascience.com/deep-learning-in-java-d9b54ae1423a
+    }
+}
---
+++ src/main/java/edu/psuti/alexandrov/stellar/StellarClass.java
@@ -0,0 +1,8 @@
+package edu.psuti.alexandrov.stellar;
+
+public enum StellarClass {
+
+    GALAXY,
+    STAR,
+    QSO
+}
